d28db6d7941e948d4cd2e786240f5665450921b4
Angelo-Netho <angelonetho@gmail.com>
2022-08-07 18:55:06 -0300

    Utilizando o Princípio da Substituição de Liskov

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -1,11 +1,7 @@
 import {Category} from "../model/Category";
+import { ICategoriesRepository, ICreateCategoryDTO } from "./ICategoriesRepository";
 
-interface ICreateCategoryDTO {
-  name: string;
-  description: string;
-}
-
-class CategoriesRepository {
+class CategoriesRepository implements ICategoriesRepository {
 
   private categories: Category[];
 
@@ -13,7 +9,7 @@ class CategoriesRepository {
     this.categories = [];
   }
 
-  create({ description, name } : ICreateCategoryDTO): void {
+  create({ name, description } : ICreateCategoryDTO): void {
     const category = new Category();
 
     Object.assign(category, {
